729c76fa19153e0a0c3060f53c30bccf63f8c9d9
Daniel Nowak <danieln311@gmail.com>
2026-07-17 17:15:46 +0000

    Edit code to initialize array inside method to remove need to outside variables

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,11 +1,12 @@
-def cipher(string, letters_list, places_to_shift)
+def cipher(string, places_to_shift)
+    letters = ("a".."z").to_a
     split_string = string.downcase.split("")
     
     encoded = split_string.map do |letter|
         if
             letter = " "
         else
-            letters_list[(letters_list.index(letter) + places_to_shift) % letters_list.length]
+            letters[(letters.index(letter) + places_to_shift) % letters.length]
         end
     end  
     encoded.join("")
