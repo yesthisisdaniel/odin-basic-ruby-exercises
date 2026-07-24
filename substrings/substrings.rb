016ab4fd03a7eeefbe58ea586dfa5d5109d7c461
Daniel Nowak <danieln311@gmail.com>
2026-07-24 09:14:43 +0000

    Finish method for exercise, using split on specific word from dictionary then filter out 0 values

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,7 +1,9 @@
+word_list = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
 def word_checker(string, dictionary)
-    case_sens_string = string.downcase.split
     
-    word_checker.each_with_object(Hash.new(0)) do |word, hash|
-        hash[word] += 1
+    dictionary.each_with_object(Hash.new(0)) do |word, hash|
+        hash[word] = string.downcase.split(word).length - 1
+        hash.delete_if { |key, value| value == 0 }
     end
 end
